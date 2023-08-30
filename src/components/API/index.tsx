8a9a4ab7469329d78c04352a10b22b3e1b0dd8a2
stvnus <stvnus98@gmail.com>
2023-08-30 10:46:23 +0700

    fix : env API

--- src/components/API/index.tsx
+++ src/components/API/index.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const apiKey = "b56bb07d6fcadcd0853ff38279104e93";
-const baseUrl = "https://api.themoviedb.org/3";
+const apiKey = process.env.NEXT_PUBLIC_TMDB_APIKEY;
+const baseUrl = process.env.NEXT_PUBLIC_TMDB_BASEURL;
 
 export const getMoviesList = async () => {
   try {
     const response = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
-    return response.data.results; // Menggunakan "results" bukan "result"
+    return response.data.results; 
   } catch (error) {
     console.error("Error fetching movie list:", error);
     return []; 
--- src/components/cardmovies/index.tsx
+++ src/components/cardmovies/index.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 interface Movie {
@@ -15,7 +16,11 @@ const CardMovie: React.FC<CardMovieProps> = ({ movie }) => {
   return (
     <div className="group w-1/5 p-4 transition-transform transform hover:scale-105">
       <div className="relative">
-        <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="rounded-lg" />
+        <img
+          src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_URL}/${movie.poster_path}`}
+          alt={movie.title}
+          className="rounded-lg"
+        />
         <div className="absolute bottom-0 left-0 right-0 px-2 py-1 bg-black bg-opacity-75 text-white rounded-b-lg">
           <div className="font-semibold">{movie.title}</div>
           <div>{movie.release_date}</div>
